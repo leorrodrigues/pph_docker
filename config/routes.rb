58b339d699ef59e51385327aff55ec15b5bc316c
leonardo <leu-rr1@hotmail.com>
2017-11-15 18:57:56 -0200

    .

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
     devise_for :users, controllers: { registrations: "registrations",:omniauth_callbacks=>"callbacks"}
-    devise_scope :user do
-        delete 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
-    end
+
     resources :posts
     root 'home#index'
 
