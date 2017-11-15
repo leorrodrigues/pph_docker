2d68d1799880701da5846894f3997389b21b3bc5
leonardo <leu-rr1@hotmail.com>
2017-11-15 19:17:03 -0200

    .

--- app/controllers/callbacks_controller.rb
+++ app/controllers/callbacks_controller.rb
@@ -7,7 +7,7 @@ class CallbacksController < Devise::OmniauthCallbacksController
           sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
           set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
         else
-          session["devise.facebook_data"] = request.env["omniauth.auth"]
+          session["devise.facebook_data"] = request.env["omniauth.auth"].except("extra")
           redirect_to new_user_registration_url
         end
     end
@@ -20,7 +20,7 @@ class CallbacksController < Devise::OmniauthCallbacksController
           sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
           set_flash_message(:notice, :success, :kind => "Github") if is_navigational_format?
         else
-          session["devise.github_data"] = request.env["omniauth.auth"]
+          session["devise.github_data"] = request.env["omniauth.auth"].except("extra")
           redirect_to new_user_registration_url
         end
     end
@@ -33,7 +33,7 @@ class CallbacksController < Devise::OmniauthCallbacksController
           sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
           set_flash_message(:notice, :success, :kind => "Twitter") if is_navigational_format?
         else
-          session["devise.twitter_data"] = request.env["omniauth.auth"]
+          session["devise.twitter_data"] = request.env["omniauth.auth"].except("extra")
           redirect_to new_user_registration_url
         end
     end
